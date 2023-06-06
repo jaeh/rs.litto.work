17a29a4d03c6f5f278f0b8def95959bdc20875a4
jaeh <jascha@jaeh.at>
2023-06-06 22:35:42 +0200

    src/pages/index: use Year component, change Picture dimensions

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -2,14 +2,10 @@ export const View = state => {
   const years = Object.entries(state.artifacts)
 
   return [
-    Picture({ name: '/img/hero', height: 1500, width: 2000 }),
+    Picture({ name: '/img/hero', height: 1000, width: 2000 }),
 
     Announcement(),
 
-    // years.map(([year, artifacts]) => [
-    //   Picture({ name: `/img/graphic_${year}`, height: 1180, width: 1920 }),
-    //   h2(year),
-    //   div(artifacts.map(ArtifactListItem)),
-    // ]),
+    years.map(([year, artifacts]) => Year({ year, artifacts })),
   ]
 }
