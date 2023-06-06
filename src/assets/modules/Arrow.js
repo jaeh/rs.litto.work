d0e22acb252203c9cd0154c0c91b54a8fbe46996
jaeh <jascha@jaeh.at>
2023-06-06 20:24:06 +0200

    assets/modules/Arrow: add float: left

--- src/assets/modules/Arrow.js
+++ src/assets/modules/Arrow.js
@@ -1,3 +1,5 @@
-export const View = state => [
-  Img({ src: '/img/arrow.png', height: 60, width: 30, alt: '' })
-]
+export const View = () => Img({ class:'Arrow', src: '/img/arrow.png', height: 60, width: 30, alt: '' })
+
+export const style = {
+  float: 'left',
+}
