3f9f881b2b2d7e8c282ddf6e944c66d3aaee7049
jaeh <jascha@jaeh.at>
2023-06-06 22:33:57 +0200

    app: change dates of exhibition artifacts, add 2023

--- src/app.mjs
+++ src/app.mjs
@@ -6,6 +6,12 @@ export const state = {
   artifacts: {
     2021: [
       {
+        title: 'SANDKASTEN, Vienna',
+        date: '2021-08-03',
+        lat: 48.2181,
+        lng: 16.3333,
+      },
+      {
         title: 'MESSEHALLE, Basel',
         date: '2021-08-27',
         lat: 47.5651,
@@ -17,38 +23,59 @@ export const state = {
         lat: 47.5779,
         lng: 7.5858,
       },
-      {
-        title: 'SANDKASTEN, Vienna',
-        date: '2021-08-03',
-        lat: 48.2181,
-        lng: 16.3333,
-      },
     ],
 
     2022: [
       {
-        title: 'SCHLANDERS, South Tirol',
+        title: 'BUNKER, Dresden',
+        date: '2022-03-07',
+        lat: 51.1135,
+        lng: 13.7531,
+      },
+      {
+        title: 'BASIS, Schlanders',
         date: '2022-07-25',
         lat: 46.6278,
         lng: 10.7662,
       },
       {
         title: 'SEMMELWEISKLINIK, Vienna',
-        date: '2022-07-25',
+        date: '2022-09-06',
         lat: 48.2362,
         lng: 16.3205,
       },
       {
-        title: 'SCHMIEDE HALLEIN, Salzburg',
-        date: '2022-07-25',
+        title: 'SCHMIEDE, Hallein',
+        date: '2022-09-17',
         lat: 47.6847,
         lng: 13.0931,
       },
+    ],
+
+    2023: [
       {
-        title: 'HELLERAU, Dresden',
-        date: '2022-07-25',
-        lat: 51.1135,
-        lng: 13.7531,
+        title: 'AA Collection, Vienna',
+        date: '2023-06-21, 19:00',
+        lat: 48.1884,
+        lng: 16.3306,
+      },
+      {
+        title: 'VZA 7, Vienna',
+        date: '2023-06-27, 11:30',
+        lat: 48.2084,
+        lng: 16.3843,
+      },
+      {
+        title: 'MuseumsQuartier Raum D, Vienna',
+        date: '2023-06-28, 19:00',
+        lat: 48.2042,
+        lng: 16.3582,
+      },
+      {
+        title: 'MEDIENWERKSTATT, Vienna',
+        date: '2023-06-30, 19:30',
+        lat: 48.2016,
+        lng: 16.3491,
       },
     ],
   },
