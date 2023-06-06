52d1171d06cbc050050492ace762ec457927e092
jaeh <jascha@jaeh.at>
2023-06-06 22:34:44 +0200

    assets/modules/Arrow: format, add styles

--- src/assets/modules/Arrow.js
+++ src/assets/modules/Arrow.js
@@ -1,5 +1,9 @@
-export const View = () => Img({ class:'Arrow', src: '/img/arrow.png', height: 60, width: 30, alt: '' })
+export const View = () =>
+  Img({ class: 'Arrow', src: '/img/arrow.png', height: 60, width: 30, alt: '' })
 
 export const style = {
   float: 'left',
+  height: '4.1em',
+  width: 'auto',
+  margin: '0 0.5em 0 0',
 }
