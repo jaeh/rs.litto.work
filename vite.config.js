e8cea2a4538d2abbc91d3fa22391d2d2dc15e083
jaeh <jascha@jaeh.at>
2024-03-10 23:37:19 +0100

    vite config: add vendor.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules') || id.includes('.svelte-kit')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
