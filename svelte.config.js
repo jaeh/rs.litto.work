863b03f7064bac225fac4d8abe34740185a95ab3
jaeh <jascha@jaeh.at>
2024-03-11 21:38:45 +0100

    update config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,22 @@
-import { config } from '@artificialmuseum/svelte-config'
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
-const svelteConfig = {
-  ...config,
+export const config = {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: '404.html',
+      precompress: true,
+    }),
+
+    alias: {
+      // this will match a file
+      $db: 'src/db/index',
+    },
+  },
 }
 
-export default svelteConfig
+export default config
